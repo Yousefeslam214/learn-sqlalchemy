621df8aa70f8d80ae94f1ad124b60b62b1923176
Yousef eslam <yousefeslam214@gmail.com>
2024-04-12 09:50:19 +0200

    sqlAlcemy

--- two/app.py
+++ two/app.py
@@ -4,47 +4,37 @@ from db import engine
 
 session = sessionmaker()(bind=engine)
 
-    #### 3 methods to add date ###
-### new_user = User(name='yousef')
-### session.add(new_user)
-### session.commit()
-##
-### user_three = User(name='yousef')
-### user_four = User(name='yousef')
-### user_five = User(name='yousef')
-### session.add_all([user_three, user_four, user_five])
-### session.commit()
-##
-##
-### users = [
-###     User(name='yousef'),
-###     User(name='mahmoud'),
-###     User(name='hassan')
-### ]
-### for user in users:
-###     session.add(user)
-###     session.commit()
+users = session.query(User).all()
+# output will be list object of "User"
+print(users)
+
+users = session.query(User).first()
+# output will be first object in list object of "User"
+print(users)
 
 
-    ## How to show data ##
-# print("##################")
-# users = session.query(User).all()
-# print(users)# data represent as a object to fix it got to class user and make __repr__ function
-# for user in users:
-#     print(f"{user.id} : {user.name}")
-# print("##################")
-# users = session.query(User).filter(User.id == 8).first()
-# print(users)
-# print("##################")
+## one() = first() = scalar() but first() is best
 
-    ## update data ##
-# update_user = session.query(User).filter(User.id == 9).first()
-# print(update_user)
-# update_user.name = "masr";
-# print(update_user)
-# print("##################")
+users = session.query(User).filter(User.id==4).one()
+# output will be spcefic object in list object of "User"
+print(users)
+users = session.query(User).filter(User.id==4).first()
+# output will be spcefic object in list object of "User"
+print(users)
+users = session.query(User).filter(User.id>4).all()
+# output will be spcefic object in list object of "User"
+print(users)
+users = session.query(User).filter(User.id!=5).all()
+# output will be spcefic object in list object of "User"
+print(users)
 
-## delete User ##
-# delete_user = session.query(User).filter(User.name == "yousef").second()
-# session.delete(delete_user)
-# session.commit()
+
+users = session.query(User).count()
+# to count the sum of object of "User"
+print(users)
+
+
+users = session.query(User).all()
+# output will be last object in list object of "User"
+print(users[::-1][0])
+print(users[-1])
